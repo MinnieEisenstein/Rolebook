0eb0d10c5600c5b3455ddf844243c7f183399913
bdrillic2 <bdrillic2@student.touro.edu>
2024-12-30 20:32:41 -0500

    Added documentation

--- src/classes/RunProgram.java
+++ src/classes/RunProgram.java
@@ -6,14 +6,19 @@ public class RunProgram {
 	public static void main(String[] args) throws StudentNotFoundException, NoStudentsException, EmptyClassException {
 		Scanner keyboard = new Scanner(System.in);
 		
+		// Prompt user for initial information
 		System.out.println("Welcome to the Rolebook Program!");
 		System.out.println("What is the teacher's name?");
 		String name = keyboard.nextLine();
 		System.out.println("What is the teacher's subject");
 		String subject = keyboard.nextLine();
 		
+		keyboard.close();
+		
+		// Create ProgramUser object
 		ProgramUser programUser = new ProgramUser(name, subject);
 		
+		// Call the runProgram method of ProgramUser to execute Rollbook functionality
 		programUser.runProgram();
 	}
 }
